5ff25725c03e810250ac9cf561b92c632912eb6a
Ahmed Dame Samb <asamb650gmail.com>
2024-04-19 12:49:55 +0200

    initial commit authentication done

--- src/main/java/com/example/projecttext/ProjecttextApplication.java
+++ src/main/java/com/example/projecttext/ProjecttextApplication.java
@@ -8,7 +8,7 @@ public class ProjecttextApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjecttextApplication.class, args);
-        System.out.println("**************AUTHENTICATION SPRING-BOOT*****************");
+        System.out.println("**************SPRING-BOOT BRESIL BURGER*****************");
     }
 
 }
